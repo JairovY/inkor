9dda6b4c9f6f26260d1d36ced4eb1aa8533d7251
JairovY <122414623+JairovY@users.noreply.github.com>
2023-09-18 04:56:40 +0200

    Add files via upload

--- juegos/logic/register_logic.php
+++ juegos/logic/register_logic.php
@@ -11,36 +11,62 @@ if ($conn->connect_error) {
 // Inicializar variables para el mensaje de error y el estado de registro
 $registerError = "";
 
-// Procesar el formulario de registro
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usernameInput = $_POST['username'];
-    $passwordInput = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hashing the password
-    $emailInput = $_POST['email'];
+// Inicializa el contador de intentos fallidos si no existe
+if (!isset($_SESSION['register_attempts'])) {
+    $_SESSION['register_attempts'] = 0;
+}
 
-    // Verificar si el nombre de usuario o el correo electrónico ya están en uso
-    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? OR email = ?");
-    $stmt->bind_param("ss", $usernameInput, $emailInput);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows > 0) {
-        $registerError = "El nombre de usuario o correo electrónico ya están en uso.";
-    } else {
-        $stmt->close();
+// Si hay más de 3 intentos fallidos, bloquea el registro durante 10 minutos
+if ($_SESSION['register_attempts'] > 3 && (time() - $_SESSION['last_attempt_time']) < 600) {
+    $registerError = "Has superado el número máximo de intentos. Por favor, espera 10 minutos antes de intentar de nuevo.";
+} else {
+    if (isset($_POST['g-recaptcha-response'])) {
+        $captcha = $_POST['g-recaptcha-response'];
+        
+        // Verificar el captcha
+        $secretKey = "TU_CLAVE_SECRETA";
+        $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$captcha");
+        $responseKeys = json_decode($response, true);
 
-        // Insertar el nuevo usuario en la base de datos
-        $stmt = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $usernameInput, $passwordInput, $emailInput);
-
-        if ($stmt->execute()) {
-            $_SESSION['message'] = "Usuario registrado con éxito!";
-            header('Location: login.php');
-            exit;
+        if (intval($responseKeys["success"]) !== 1) {
+            $registerError = "Por favor, verifica el captcha.";
         } else {
-            $registerError = "Hubo un error al registrar el usuario.";
-        }
-    }
+            // Procesar el formulario de registro
+            $usernameInput = $_POST['username'];
+            $passwordInput = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hashing the password
+            $emailInput = $_POST['email'];
 
-    $stmt->close();
-    $conn->close();
+            // Verificar si el nombre de usuario o el correo electrónico ya están en uso
+            $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? OR email = ?");
+            $stmt->bind_param("ss", $usernameInput, $emailInput);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                $registerError = "El nombre de usuario o correo electrónico ya están en uso.";
+                // Incrementa el contador de intentos fallidos y registra el tiempo del último intento
+                $_SESSION['register_attempts']++;
+                $_SESSION['last_attempt_time'] = time();
+            } else {
+                $stmt->close();
+
+                // Insertar el nuevo usuario en la base de datos
+                $stmt = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+                $stmt->bind_param("sss", $usernameInput, $passwordInput, $emailInput);
+
+                if ($stmt->execute()) {
+                    $_SESSION['message'] = "Usuario registrado con éxito!";
+                    header('Location: login.php');
+                    exit;
+                } else {
+                    $registerError = "Hubo un error al registrar el usuario.";
+                }
+            }
+
+            $stmt->close();
+            $conn->close();
+        }
+    } else {
+        $registerError = "Por favor, verifica el captcha.";
+    }
 }
 ?>
--- juegos/views/register_view.php
+++ juegos/views/register_view.php
@@ -50,7 +50,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <div class="g-recaptcha" data-sitekey="6LdBODMoAAAAACmTpPR5s01aewC-6qdq0AK_bVgW"></div>
 
     </div>
     <?php include 'views/footer_view.php'; ?>
